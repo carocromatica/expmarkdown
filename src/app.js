39cd88d399424874a9950fb435c1cab316a1f238
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-08-27 13:21:10 -0300

    funcion completa

--- src/app.js
+++ src/app.js
@@ -1,38 +1,49 @@
 const path = require('path');
 const fetch = require('node-fetch');
 fs = require('fs')
-const Marked = require('marked');
 
 const getmd = path.resolve('src/README.md');
 fs.readFile(getmd, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-
-getLinks(data);
+  markdownLinkExtractor(data);
 });
 
-function getLinks(markdown) {
+const Marked = require('marked');
+
+// Función necesaria para extraer los links usando marked
+// (tomada desde biblioteca del mismo nombre y modificada para el ejercicio)
+// Recibe texto en markdown y retorna sus links en un arreglo
+function markdownLinkExtractor(markdown) {
   const links = [];
+
   const renderer = new Marked.Renderer();
-  
+
+  // Taken from https://github.com/markedjs/marked/issues/1279
+  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+
+  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+
   renderer.link = function(href, title, text) {
-
-    let url=href;
-
     links.push({
       href: href,
       text: text,
-      title: title
+      title: title,
     });
-
-    console.log(url);
   };
-
-  
-  
+  renderer.image = function(href, title, text) {
+      // Remove image size at the end, e.g. ' =20%x50'
+      href = href.replace(/ =\d*%?x\d*%?$/, '');
+      links.push({
+        href: href,
+        text: text,
+        title: title,
+      });
+  };
   Marked(markdown, {renderer: renderer});
-  
-  
-  
+
+  console.log(links)
 };
